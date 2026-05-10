5882c0df3e38a65b45ed2241c7a07539e584bcf2
himanshu chaturvedi <himanshu.chaturvedi@ourshopee.com>
2026-05-10 20:16:16 +0530

    done

--- components/propertyDetails/ExtraInfo.jsx
+++ components/propertyDetails/ExtraInfo.jsx
@@ -1,10 +1,35 @@
 "use client";
 
+import * as AiIcons from "react-icons/ai";
+import * as BiIcons from "react-icons/bi";
+import * as BsIcons from "react-icons/bs";
+import * as CgIcons from "react-icons/cg";
+import * as CiIcons from "react-icons/ci";
+import * as DiIcons from "react-icons/di";
+import * as FaLegacyIcons from "react-icons/fa";
 import * as FiIcons from "react-icons/fi";
 import * as FaIcons from "react-icons/fa6";
+import * as FcIcons from "react-icons/fc";
+import * as GiIcons from "react-icons/gi";
+import * as GoIcons from "react-icons/go";
+import * as GrIcons from "react-icons/gr";
+import * as HiIcons from "react-icons/hi";
+import * as Hi2Icons from "react-icons/hi2";
+import * as ImIcons from "react-icons/im";
+import * as IoLegacyIcons from "react-icons/io";
 import * as MdIcons from "react-icons/md";
 import * as IoIcons from "react-icons/io5";
-import * as BsIcons from "react-icons/bs";
+import * as LiaIcons from "react-icons/lia";
+import * as LuIcons from "react-icons/lu";
+import * as PiIcons from "react-icons/pi";
+import * as RiIcons from "react-icons/ri";
+import * as RxIcons from "react-icons/rx";
+import * as SiIcons from "react-icons/si";
+import * as SlIcons from "react-icons/sl";
+import * as TbIcons from "react-icons/tb";
+import * as TfiIcons from "react-icons/tfi";
+import * as VscIcons from "react-icons/vsc";
+import * as WiIcons from "react-icons/wi";
 
 const getLabel = (value) => {
   if (!value) return "";
@@ -28,7 +53,41 @@ const formatCount = (value) => {
 const getReactIconComponent = (iconName) => {
   const key = String(iconName || "").trim();
   if (!key) return null;
-  const packs = [FiIcons, FaIcons, MdIcons, IoIcons, BsIcons];
+
+  // Support multiple react-icons packs used historically in CMS content.
+  const packs = [
+    AiIcons,
+    BiIcons,
+    BsIcons,
+    CgIcons,
+    CiIcons,
+    DiIcons,
+    FiIcons,
+    FaIcons, // react-icons/fa6
+    FaLegacyIcons, // react-icons/fa
+    FcIcons,
+    GiIcons,
+    GoIcons,
+    GrIcons,
+    HiIcons, // react-icons/hi
+    Hi2Icons, // react-icons/hi2
+    ImIcons,
+    MdIcons,
+    IoIcons, // react-icons/io5
+    IoLegacyIcons, // react-icons/io
+    LiaIcons,
+    LuIcons,
+    PiIcons,
+    RiIcons,
+    RxIcons,
+    SiIcons,
+    SlIcons,
+    TbIcons,
+    TfiIcons,
+    VscIcons,
+    WiIcons,
+  ];
+
   for (const pack of packs) {
     if (pack[key]) return pack[key];
   }
