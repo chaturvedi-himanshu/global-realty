d4fb7b9fb2f5b62d58a7af44c937ee983e8155a6
himanshu chaturvedi <himanshu.chaturvedi@ourshopee.com>
2026-05-26 15:41:40 +0530

    d

--- components/propertyDetails/Features.jsx
+++ components/propertyDetails/Features.jsx
@@ -3,63 +3,21 @@ import React from "react";
 export default function Features({ amenities = [], features = [] }) {
   if (!amenities.length && !features.length) return null;
 
-  // Chunk amenities into groups of ~5 for the 3-column layout
-  const chunkSize = Math.ceil(amenities.length / 3) || 5;
-  const col1 = amenities.slice(0, chunkSize);
-  const col2 = amenities.slice(chunkSize, chunkSize * 2);
-  const col3 = amenities.slice(chunkSize * 2);
-
   return (
     <>
       <div className="wg-title text-11 fw-6 text-color-heading">
         Amenities And Features
       </div>
-      <div className="wrap-feature">
-        {col1.length > 0 && (
-          <div className="box-feature">
-            <ul>
-              {col1.map((a) => (
-                <li className="feature-item" key={a._id || a.name}>
-                  {a.name}
-                </li>
-              ))}
-            </ul>
+      <div className="det-amenities-grid" role="list">
+        {amenities.map((a) => (
+          <div
+            className="det-amenity-item feature-item"
+            key={a._id || a.name}
+            role="listitem"
+          >
+            {a.name}
           </div>
-        )}
-        {col2.length > 0 && (
-          <div className="box-feature">
-            <ul>
-              {col2.map((a) => (
-                <li className="feature-item" key={a._id || a.name}>
-                  {a.name}
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
-        {col3.length > 0 && (
-          <div className="box-feature">
-            <ul>
-              {col3.map((a) => (
-                <li className="feature-item" key={a._id || a.name}>
-                  {a.name}
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
-        {/* Extra custom features (key-value pairs) */}
-        {/* {features.length > 0 && (
-          <div className="box-feature">
-            <ul>
-              {features.map((f, i) => (
-                <li className="feature-item" key={i}>
-                  {f.label}: {f.value}
-                </li>
-              ))}
-            </ul>
-          </div>
-        )} */}
+        ))}
       </div>
     </>
   );
